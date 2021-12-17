e1597b330a0c7b79784a40b99b1dda4b3267ad8b
ethon92 <e19920526@gmail.com>
2021-12-17 17:30:02 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
+
+// 載入mongoose
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
+
+// 與mongodb連線
+mongoose.connect('mongodb://localhost/todo_list')
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+// 連線異常時提示訊息
+db.on('error', () => {
+  console.log('mongodb error!!')
+})
+
+// 連線成功時提示訊息
+db.once('open', () => {
+  console.log('mongodb connected!!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world!!')
---
+++ models/todo.js
@@ -0,0 +1,16 @@
+// 引入mongoose
+const mongoose = require('mongoose')
+
+// 引入mongoose.Schema模組
+const Schema = mongoose.Schema
+
+// 建構新的Schema
+const todoSchema = new Schema({
+  name: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  }
+})
+
+// 利用module.exports將Schema輸出並命名為'Todo'
+module.exports = mongoose.model('Todo', todoSchema)
