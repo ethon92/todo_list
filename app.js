92a00aa00bfa44d317e004e6ee91906893fc9e9f
ethon92 <e19920526@gmail.com>
2021-12-18 14:37:33 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const { engine } = require('express/lib/application')
 
+// 載入Todo model
+const Todo = require('./models/todo')
+
 const app = express()
 const port = 3000
 
@@ -30,7 +33,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出Todo model中所有資料
+    .lean() // 將mongoose model物件轉換成javascript陣列
+    .then(todos => res.render('index', { todos }) ) // 將資料傳入index
+    .catch(error => console.error(error)) // 若發生錯誤時提示錯誤訊息
 })
 
 app.listen(port, () => {
