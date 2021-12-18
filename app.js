3f09f9b837ce9c5cfced86bdf2513f9bbd4103c8
ethon92 <e19920526@gmail.com>
2021-12-18 15:25:25 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const exphbs = require('express-handlebars')
-const { engine } = require('express/lib/application')
+// const { engine } = require('express/lib/application')
+
+// 載入 body-parser
+const bodyParser = require('body-parser')
 
 // 載入Todo model
 const Todo = require('./models/todo')
@@ -32,11 +35,27 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find() // 取出Todo model中所有資料
     .lean() // 將mongoose model物件轉換成javascript陣列
     .then(todos => res.render('index', { todos }) ) // 將資料傳入index
     .catch(error => console.error(error)) // 若發生錯誤時提示錯誤訊息
+})
+
+// 設定新增todo頁面
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+// 設定新增todo後的行為
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 將name存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
