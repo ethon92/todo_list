3b055613df31bc8a133be80255ae7a2abc3ddff5
ethon92 <e19920526@gmail.com>
2021-12-22 16:00:57 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() // 取出Todo model中所有資料
     .lean() // 將mongoose model物件轉換成javascript陣列
+    .sort({ _id: "asc" }) // asc為升冪排序(ascending)，desc為降冪排序(descending)
     .then(todos => res.render('index', { todos }) ) // 將資料傳入index
     .catch(error => console.error(error)) // 若發生錯誤時提示錯誤訊息
 })
@@ -67,7 +68,7 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => console.log(error)) // 錯誤提示訊息
 })
 
-// 設定修改資料頁面
+// 設定顯示修改資料頁面
 app.get('/todos/:id/edit', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
